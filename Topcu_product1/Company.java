15ad41635a70f5b4d4e5c0c86c4fddb8608e869e
Mathias <moraisen@me.com>
2016-05-04 15:48:27 +0200

    new file:   Topcu_product1/Company.java

---
+++ Topcu_product1/Company.java
@@ -0,0 +1,14 @@
+package at.olm.bak.git;
+
+import java.util.ArrayList;
+
+public class Company {
+
+	private ArrayList<product> products;
+	
+	public Company()
+	{
+		products = new ArrayList() ArrayList<product>;
+		
+	}
+}
