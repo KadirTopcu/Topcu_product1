746867670e39af50a01195bba81f02e5226ca576
Kadir <kadir@me.com>
2016-05-04 15:24:27 +0200

    Product Product

---
+++ product.java
@@ -0,0 +1,18 @@
+package at.olm.bak.git;
+
+public class Product {
+	private String name;
+	
+	public product(String name) {
+		setName(name);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
